9d14cb8b491f660e77f1789d0f549f2cd3e0408e
Gary Llil <garyjllil@gmail.com>
2020-02-28 23:42:31 -0500

    redid the entire portfolio page and added more detailed information finished the contact me page also.

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-var instance = M.Carousel.init({
-    fullWidth: true
-  });
+// var instance = M.Carousel.init({
+//     fullWidth: true
+//   });
+  
 
 $(document).ready(function(){
     M.AutoInit();
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+    instances
     $('.sidenav').sidenav();
-    $('.carousel').carousel();
-    $('.carousel.carousel-slider').carousel({
-        fullWidth: true,
-        indicators: true
-      });
+    // $('.carousel').carousel();
+    // $('.carousel.carousel-slider').carousel({
+    //     fullWidth: true,
+    //     indicators: true
+    //   });
+    
+    $('#textarea1').val('New Text');
+    M.textareaAutoResize($('#textarea1'));
   });
+
+
 
 
 
